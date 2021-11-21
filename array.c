4cf9189308bb76706d4284e90654f390d4cc2351
YuriiVid <yuriividoniak@gmail.com>
2021-11-21 15:42:37 +0200

    Cosmetic changes

--- array.c
+++ array.c
@@ -4,8 +4,12 @@ int main(void)
 {
     int A[12] = {5, 9, 15, 21, 2, 6, -12, 26, -8, 1, -5, -19}, odd_2, k = 0;
     double C[12];
+
+
     for (int i = 0; i < 12; i++)
     {
+
+
         if (A[i] % 2 != 0)
         {
             k++;
@@ -14,28 +18,40 @@ int main(void)
                 odd_2 = A[i];
             }
         }
+
+
         if (k > 1)
         {
             C[i] = ((double)A[i]) / odd_2;
         }
+
+
         else
         {
             C[i] = A[i];
         }
     }
+
+
     if (k == 0)
     {
         printf("There's no odd numbers in array\n");
     }
+
+
     if (k == 1)
     {
         printf("There's only one odd number in array\n");
     }
+
+
     printf("-----Array-----\n");
     for (int i = 0; i < 12; i++)
     {
         printf("A[%d] = %d\n", i, A[i]);
     }
+
+    
     printf("-----Newly formed array-----\n");
     for (int i = 0; i < 12; i++)
     {
